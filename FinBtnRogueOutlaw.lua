d11ffd3d61d1da61e7646643cd105fcd26928736
celaena <arona4000@gmail.com>
2016-07-09 18:53:17 -0500

    Basic skill+logic reconfig for outlaw rogue

--- FinBtnRogueOutlaw.lua
+++ FinBtnRogueOutlaw.lua
@@ -1,112 +1,156 @@
 -------------------------------------------
 -- Outlaw Rogue Module --
 -------------------------------------------
-local module = {}
+local module = {};
 local moduleName = "RogueOutlaw";
 FinBtn[moduleName] = module;
 
-local main = FinBtn.Main
+local main = FinBtn.Main;
 
-local SLICE_AND_DICE = "Slice and Dice"
-local EVISCERATE = "Eviscerate"
-local SINISTER_STRIKE = "Sinister Strike"
-local REVEALING_STRIKE = "Revealing Strike"
-local KICK = "Kick"
-local CRIMSON_TEMPEST = "Crimson Tempest"
+local ADRENALINE_RUSH = "Adrenaline Rush";
+local AMBUSH = "Ambush";
+local BETWEEN_THE_EYES = "Between the Eyes";
 local BLADE_FLURRY = "Blade Flurry"
-local KILLING_SPREE = "Killing Spree"
-local ADRENALINE_RUSH = "Adrenaline Rush"
-local VANISH = "Vanish"
-
-local SIN = "Sin";
-local REV =	"Rev";
-local EV = "Ev";
-local SND = "SnD";
+local CANNONBALL_BARRAGE = "Cannonball Barrage";
+local DEATH_FROM_ABOVE = "Death from Above";
 local KICK = "Kick";
-local KS = "KS";
-local AD = "AD";
-local VAN = "Van";
-local CT = "CT";
-local BF = "BF";
+local PISTOL_SHOT = "Pistol Shot";
+local ROLL_THE_BONES = "Roll the Bones";
+local RUN_THROUGH = "Run Through";
+local SABER_SLASH = "Saber Slash";
+local VANISH = "Vanish";
+local GHOSTLY_STRIKE = "Ghostly Strike";
+local MARKED_FOR_DEATH = "Marked for Death";
+local SLICE_AND_DICE = "Slice and Dice";
+local KILLING_SPREE = "Killing Spree";
 
-local bf_max = 8
-local ct_max = 10
+local SHARK_INFESTED_WATERS = "Shark Infested Waters";
+local GRAND_MELEE = "Grand Melee";
+local JOLLY_ROGER = "Jolly Roger";
+local BURIED_TREASURE = "Buried Treasure";
+local TRUE_BEARING = "True Bearing";
+local BROADSIDES = "Broadsides";
+
+local AR = "AR";
+local AM = "Am";
+local BTE = "BtE";
+local BF = "BF";
+local CB = "CB";
+local DFA = "DfA";
+local KICK = "Kick";
+local PS = "PS";
+local RTB = "RtB";
+local RT = "RT";
+local SS = "SS";
+local VAN = "Van";
+local GS = "GS";
+local MFD = "MfD";
+local SAD = "SaD";
+local KS = "KS";
+
+local bf_max = 8;
 
 module.SKILLS = {
-	[SND] = {
+	[AR] = {
 		[main.TYPE] = main.SPELL,
-		[main.NAME] = SLICE_AND_DICE
+		[main.NAME] = ADRENALINE_RUSH
 	},
-	[EV] = {
+	[AM] = {
 		[main.TYPE] = main.SPELL,
-		[main.NAME] = EVISCERATE
+		[main.NAME] = AMBUSH
 	},
-	[SIN] = {
+	[BTE] = {
 		[main.TYPE] = main.SPELL,
-		[main.NAME] = SINISTER_STRIKE
-	},
-	[REV] = {
-		[main.TYPE] = main.SPELL,
-		[main.NAME] = REVEALING_STRIKE
-	},
-	[KICK] = {
-		[main.TYPE] = main.SPELL,
-		[main.NAME] = KICK
-	},
-	[CT] = {
-		[main.TYPE] = main.SPELL,
-		[main.NAME] = CRIMSON_TEMPEST
+		[main.NAME] = BETWEEN_THE_EYES
 	},
 	[BF] = {
 		[main.TYPE] = main.SPELL,
 		[main.NAME] = BLADE_FLURRY
 	},
-	[KS] = {
+	[CB] = {
 		[main.TYPE] = main.SPELL,
-		[main.NAME] = KILLING_SPREE
+		[main.NAME] = CANNONBALL_BARRAGE
 	},
-	[AD] = {
+	[DFA] = {
 		[main.TYPE] = main.SPELL,
-		[main.NAME] = ADRENALINE_RUSH
+		[main.NAME] = DEATH_FROM_ABOVE
+	},
+	[KICK] = {
+		[main.TYPE] = main.SPELL,
+		[main.NAME] = KICK
+	},
+	[PS] = {
+		[main.TYPE] = main.SPELL,
+		[main.NAME] = PISTOL_SHOT
+	},
+	[RTB] = {
+		[main.TYPE] = main.SPELL,
+		[main.NAME] = ROLL_THE_BONES
+	},
+	[RT] = {
+		[main.TYPE] = main.SPELL,
+		[main.NAME] = RUN_THROUGH
+	},
+	[SS] = {
+		[main.TYPE] = main.SPELL,
+		[main.NAME] = SABER_SLASH
 	},
 	[VAN] = {
 		[main.TYPE] = main.SPELL,
 		[main.NAME] = VANISH
+	},
+	[GS] = {
+		[main.TYPE] = main.SPELL,
+		[main.NAME] = GHOSTLY_STRIKE
+	},
+	[MFD] = {
+		[main.TYPE] = main.SPELL,
+		[main.NAME] = MARKED_FOR_DEATH
+	},
+	[SAD] = {
+		[main.TYPE] = main.SPELL,
+		[main.NAME] = SLICE_AND_DICE
+	},
+	[KS] = {
+		[main.TYPE] = main.SPELL,
+		[main.NAME] = KILLING_SPREE
 	}
-}
+};
 
 module.SKILL_ORDER = {
-	SIN,
-	REV,
-	EV,
-	SND,
+	SS,
+	PS,
+	RT,
+	DFA,
+	RTB,
+	BTE,
 	KICK,
-	KS,
-	AD,
+	AR,
+	CB,
 	VAN,
-	CT,
 	BF
-}
+};
 
 local function EvaluateButtons(buttons)
-	local _,gcdLeft,_ = GetSpellCooldown(SINISTER_STRIKE);
-
-	local _,_,_,_,_,_,expiresSD,_,_,_,_,_,_,_,_,_ = UnitAura(main.PLAYER,SLICE_AND_DICE,nil,main.PLAYER_HELPFUL)
-	if (expiresSD == nil or (expiresSD - GetTime()) < 8) then
-		buttons[SND]:SetAlpha(main.ON)
-		buttons[EV]:SetAlpha(main.OFF)	
-	else
-		buttons[SND]:SetAlpha(main.OFF)
-		buttons[EV]:SetAlpha(main.ON)
-	end
+	local _,gcdLeft,_ = GetSpellCooldown(SABER_SLASH);
 	
-	local _,_,_,_,_,_,expiresRS,_,_,_,_,_,_,_,_,_ = UnitAura(main.TARGET,REVEALING_STRIKE,nil,main.PLAYER_HARMFUL)
-	if (expiresRS ~= nil and expiresRS > gcdLeft) then
-		buttons[SIN]:SetAlpha(main.ON)
-		buttons[REV]:SetAlpha(main.OFF)
+	local _,_,_,_,_,_,expiresJR,_,_,_,_,_,_,_,_,_ = UnitAura(main.PLAYER,JOLLY_ROGER,nil,main.PLAYER_HELPFUL);
+	local _,_,_,_,_,_,expiresBr,_,_,_,_,_,_,_,_,_ = UnitAura(main.PLAYER,BROADSIDES,nil,main.PLAYER_HELPFUL);
+	local _,_,_,_,_,_,expiresTB,_,_,_,_,_,_,_,_,_ = UnitAura(main.PLAYER,TRUE_BEARING,nil,main.PLAYER_HELPFUL);
+	local _,_,_,_,_,_,expiresGM,_,_,_,_,_,_,_,_,_ = UnitAura(main.PLAYER,GRAND_MELEE,nil,main.PLAYER_HELPFUL);
+	local _,_,_,_,_,_,expiresBT,_,_,_,_,_,_,_,_,_ = UnitAura(main.PLAYER,BURIED_TREASURE,nil,main.PLAYER_HELPFUL);
+	local _,_,_,_,_,_,expiresSIF,_,_,_,_,_,_,_,_,_ = UnitAura(main.PLAYER,SHARK_INFESTED_WATERS,nil,main.PLAYER_HELPFUL);
+	local expiresRTB = expiresJR or expiresBr or expiresTB or expiresGM or expiresBT or expiresSIF;	
+	if (expiresRTB == nil or (expiresRTB - GetTime()) < 8) then
+		buttons[RTB]:SetAlpha(main.ON);
+		buttons[RT]:SetAlpha(main.OFF);	
+		buttons[BTE]:SetAlpha(main.OFF);
+		buttons[DFA]:SetAlpha(main.OFF);	
 	else
-		buttons[SIN]:SetAlpha(main.OFF)
-		buttons[REV]:SetAlpha(main.ON)		
+		buttons[RTB]:SetAlpha(main.OFF);
+		buttons[RT]:SetAlpha(main.ON);
+		buttons[BTE]:SetAlpha(main.ON);
+		buttons[DFA]:SetAlpha(main.ON);
 	end
 	
 	local _,_,_,_,_,_,_,_,interrupt = UnitCastingInfo(main.TARGET)
@@ -116,21 +160,15 @@ local function EvaluateButtons(buttons)
 		buttons[KICK]:SetAlpha(main.ON)
 	end
 	
-	local ctSetting = main.HALF;
 	local bfSetting = main.HALF;
 	local minRange, maxRange = main.rc:GetRange(main.MOUSEOVER)
 	if (minRange and maxRange) then
-		if (ct_max <= minRange) then
-			-- Both off
-		elseif (bf_max <= minRange and ct_max >= maxRange) then
-			ctSetting = main.ON;
+		if (bf_max <= minRange) then
 			bfSetting = main.HALF;
 		else
-			ctSetting = main.ON;
 			bfSetting = main.ON;
 		end
 	end
-	buttons[CT]:SetAlpha(ctSetting);
 	buttons[BF]:SetAlpha(bfSetting);
 end
 
